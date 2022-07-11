caa0aaa499a657ebb8643506b43e8e805f9ffbcc
Nick Kalenichenko <nickkalenichenko@MacBook-Pro-4.local>
2022-07-11 10:44:59 -0400

    added package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='demo',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/nick-kalen/DS5100---package',
+      author='Nick and Hyun',
+      author_email='test@gmail.com',
+      license='Apache License 2.0',
+      packages=['demo'])
